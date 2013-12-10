bec47bc11c463ea6880d9d41b605f81638d81eb1
danbennett <danbennett86@yahoo.com>
2013-12-10 00:57:24 +0000

    Added typhoon assembly, added burger icon asset, removed tab bar controller, added container view

---
+++ Auditr/Assembly/DBAssembly.h
@@ -0,0 +1,14 @@
+//
+//  DBAssembly.h
+//  Auditr
+//
+//  Created by Daniel Bennett on 10/12/2013.
+//  Copyright (c) 2013 Daniel Bennett. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Typhoon/Typhoon.h>
+
+@interface DBAssembly : TyphoonAssembly
+
+@end
---
+++ Auditr/Assembly/DBAssembly.m
@@ -0,0 +1,13 @@
+//
+//  DBAssembly.m
+//  Auditr
+//
+//  Created by Daniel Bennett on 10/12/2013.
+//  Copyright (c) 2013 Daniel Bennett. All rights reserved.
+//
+
+#import "DBAssembly.h"
+
+@implementation DBAssembly
+
+@end
